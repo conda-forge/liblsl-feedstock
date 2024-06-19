6897fbacd2ce0804432335bd7efa34945f17873b
Eric Larson <larson.eric.d@gmail.com>
2024-06-19 12:03:35 -0400

    FIX: Std

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,6 @@ cmake $CMAKE_ARGS \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DLSL_BUNDLED_PUGIXML=OFF \
 	-DLSL_UNIXFOLDERS=ON \
+	-DCMAKE_CXX_STANDARD=11 \
 	-S . -B build
 cmake --build build --config Release -j --target install
