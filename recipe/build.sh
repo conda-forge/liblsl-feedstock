4e504d315f15c385727d5254a80949cf2d93e9cb
Chadwick Boulay <chadwick.boulay@gmail.com>
2026-01-11 12:22:56 -0500

    Bump build

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,7 @@ cmake $CMAKE_ARGS \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DLSL_BUNDLED_PUGIXML=OFF \
 	-DLSL_UNIXFOLDERS=ON \
-	-DCMAKE_CXX_STANDARD=11 \
+	-DLSL_FRAMEWORK=OFF \
+	-DCMAKE_CXX_STANDARD=20 \
 	-S . -B build
 cmake --build build --config Release -j --target install
